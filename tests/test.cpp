c479c1e7c97b735ccc090e8ce6e587e362178f57
Gunqqer <gunqqer@friithian.dev>
2021-05-07 22:29:53 -0400

    Switch to using assert for tests, create initial tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,10 @@
 #include "../StreamDye.hpp"
-
+#include <assert.h>
 int main()
 {
 	//TODO actually write tests, integrate with cmake
-	std::string testString{"Hello!\n"};
-	std::cout <<StreamDye::printColor(testString, StreamDye::Color::red);
-	std::cout <<StreamDye::printEffect(testString, StreamDye::Effect::bold);
+	std::string testString{"Hello!"};
+	assert(StreamDye::printColor(testString, StreamDye::Color::red) == "\033[31mHello!\033[39m");
+	assert(StreamDye::printEffect(testString, StreamDye::Effect::bold) == "\033[1mHello!\033[21m");
 
 }
