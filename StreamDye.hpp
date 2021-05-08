3a07971fff916cc51217c19e0abd48a186dbbaf9
Gunqqer <gunqqer@friithian.dev>
2021-05-07 21:42:32 -0400

    add effects related functions

--- StreamDye.hpp
+++ StreamDye.hpp
@@ -11,9 +11,9 @@ concept Printable = requires(T a)
 };
 
 //global constants
-constexpr char prefix[]{"\x1B["}; //needs a code and postfix appended
-constexpr char postfix[]{"m"};
-constexpr char seperator[]{";"}; //Used after prefix and code to add more codes in special ways
+constexpr char prefix[]{"\x1B["}; //Needs a code and postfix appended to actually do anything, mostly for internal use
+constexpr char postfix[]{"m"}; //Mostly for internal use
+constexpr char seperator[]{";"}; //Used after prefix and code to add more codes in special ways, mostly for internal use
 constexpr char clear[]{"\033[0m"}; //resets all effects, output this to a stream to clear all ANSI codes
 
 
@@ -42,7 +42,8 @@ enum class BrightColor
 	brightWhite = 97
 };
 //Not all effects will be supported by your terminal emulator, be careful when using these
-enum class Effects
+constexpr int effectResetOffset = 20;
+enum class Effect
 {
 	bold = 1,
 	faint = 2, //Not widely supported
@@ -53,15 +54,19 @@ enum class Effects
 	reverseForegroundBackground = 7, //also known as [[reverse video]]
 	conceal = 8, //Not widely supported
 	crossedOut = 9, //Not widely supported
+	//Off codes are the on codes +20
 	boldOff = 21, //Not widely supported
 	faintOff = 22, //Also turns off color
 	italicOff = 23,
 	underlineOff = 24,
 	blinkOff = 25,
+	blinkFastOff = 25, //Codes for resetting both blinks are the same
 	reverseForegroundBackgroundOff = 27,
 	concealOff = 28,
 	crossedOutOff = 29
 };
+
+inline std::string resetAll(){return clear;}
 
 //These are pairs of functions that work together
 //The first returns a string that will enable the code and must be manually disabled
@@ -93,6 +98,19 @@ template<Printable P> std::string printRgbColor(P obj, int red, int green, int b
 {
 	std::stringstream output;
 	output <<rgbColor(red, green, blue) <<obj <<Color::reset;
+	return output.str();
+}
+
+inline std::string effect(Effect e)
+{
+	std::stringstream output;
+	output <<prefix <<static_cast<int>(e) <<postfix;
+	return output.str();
+}
+template<Printable P> std::string printEffect(P obj, Effect e)
+{
+	std::stringstream output;
+	output <<effect(e) <<obj <<static_cast<int>(e)+effectResetOffset;
 	return output.str();
 }
 
