52708114e2819234351dfdac285d47d003383200
Gunqqer <gunqqer@friithian.dev>
2021-05-07 23:09:47 -0400

    Slightly change how tests are done to be more human readable

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,10 +1,20 @@
 #include "../StreamDye.hpp"
 #include <assert.h>
+#include <vector>
+#include <utility>
+#include <iostream>
+
 int main()
 {
 	//TODO actually write tests, integrate with cmake
 	std::string testString{"Hello!"};
-	assert(StreamDye::printColor(testString, StreamDye::Color::red) == "\033[31mHello!\033[39m");
-	assert(StreamDye::printEffect(testString, StreamDye::Effect::bold) == "\033[1mHello!\033[21m");
+	std::vector<std::pair<std::string, std::string>> printTest;
+	printTest.push_back(std::make_pair(StreamDye::printColor(testString, StreamDye::Color::red), "Red"));
+	printTest.push_back(std::make_pair(StreamDye::printEffect(testString, StreamDye::Effect::bold), "Bold"));
+	printTest.push_back(std::make_pair(StreamDye::printEffect(testString, StreamDye::Effect::blinkSlow), "Blinking"));
+	assert(printTest[0].first == "\033[31mHello!\033[39m");
+	assert(printTest[1].first == "\033[1mHello!\033[21m");
+	assert(printTest[2].first == "\033[5mHello!\033[25m");
+	for(auto& i: printTest){std::cout <<i.second <<": " <<i.first <<std::endl;}
 
 }
