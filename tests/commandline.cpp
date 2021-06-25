8011567b1238df5d80466e0a14cc92abbef04138
Gunqqer <gunqqer@friithian.dev>
2021-06-25 14:23:42 -0400

    Make test output better

--- tests/commandline.cpp
+++ tests/commandline.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	std::string output;
 	for(int i = 1; i < argc; i++)
 	{
-		output.append(argv[i]);
+		output.append(argv[i]).append(" ");
 	}
-	std::cout <<StreamDye::effectPrint(output, StreamDye::Effect::conceal)<<std::endl;
+	std::cout <<StreamDye::effectPrint(output, StreamDye::Effect::blinkSlow)<<std::endl;
 }
